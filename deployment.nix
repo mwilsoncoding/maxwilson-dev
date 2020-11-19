bada95f947b45a654e99ffe5296dc51485842087
Max Wilson <max@maxwilson.dev>
2020-11-19 18:06:01 -0500

    ingress attempt

--- deployment.nix
+++ deployment.nix
@@ -71,7 +71,7 @@ in
       mode = "bridge";
     };
     networking.interfaces.mv-eth1.ipv4.addresses = [ { address = "10.0.1.2"; prefixLength = 24; } ];
-    networking.interfaces.eth1.ipv4.addresses = mkForce [];
+    networking.interfaces.eth1.ipv4.addresses = lib.mkForce [];
     networking.interfaces.mv-eth1-host.ipv4.addresses = [ { address = "10.0.1.0"; prefixLength = 24; } ];
     #networking.bridges.br0.interfaces = [];
     #networking.interfaces.br0.ipv4.addresses = [ { address = "10.0.1.0"; prefixLength = 24; } ];
