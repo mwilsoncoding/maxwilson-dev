bd7d9829809c4fcbb7229923a1c91fc24b60f31b
Max Wilson <max@maxwilson.dev>
2020-11-19 16:43:05 -0500

    ingress attempt

--- deployment.nix
+++ deployment.nix
@@ -19,35 +19,35 @@ in
       
   cluster-node-0 = { config, resources, lib, ... }: {
     # Configure main ingress at this level since you have access to what containers exist
-#    containers.site-i.config =
-#      let
-#        inherit resources;
-#      in
-#      { config, pkgs, lib, ... }:
-#      {
-##        security.acme.email = "maxwilsondotdev+acmecerts@${domain}";
-#        networking.firewall.allowedTCPPorts = [ 80 443 ];
-#        networking.interfaces.mv-eth1.ipv4.addresses = [{address = "10.0.1.1"; prefixLength = 24;}];
-#        services.nginx.enable = true;
-#        services.nginx.recommendedGzipSettings = true;
-#        services.nginx.recommendedOptimisation = true;
-#        services.nginx.recommendedProxySettings = true;
-##        services.nginx.recommendedTlsSettings = true;
-#        services.nginx.virtualHosts.site-i = {
-##          forceSSL = true;
-##          enableACME = true;
-#          serverAliases = [ "www.${domain}" "${domain}" ];
-#          locations."/" = {
-#            proxyPass = "http://site-upstream";
-#          };
-#        };
-#        services.nginx.upstreams.site-upstream.servers = {
-#          site-0 = {};
-#        };
-#      };
-#    containers.site-i.forwardPorts = [{hostPort = 80;} {hostPort = 443;}];
-#    containers.site-i.autoStart = true;
-#    containers.site-i.macvlans = ["eth1"];
+    containers.site-i.config =
+      let
+        inherit resources;
+      in
+      { config, pkgs, lib, ... }:
+      {
+#        security.acme.email = "maxwilsondotdev+acmecerts@${domain}";
+        networking.firewall.allowedTCPPorts = [ 80 443 ];
+        networking.interfaces.mv-eth1.ipv4.addresses = [{address = "10.0.1.1"; prefixLength = 24;}];
+        services.nginx.enable = true;
+        services.nginx.recommendedGzipSettings = true;
+        services.nginx.recommendedOptimisation = true;
+        services.nginx.recommendedProxySettings = true;
+#        services.nginx.recommendedTlsSettings = true;
+        services.nginx.virtualHosts.site-i = {
+#          forceSSL = true;
+#          enableACME = true;
+          serverAliases = [ "www.${domain}" "${domain}" ];
+          locations."/" = {
+            proxyPass = "http://site-upstream";
+          };
+        };
+        services.nginx.upstreams.site-upstream.servers = {
+          site-0 = {};
+        };
+      };
+    containers.site-i.forwardPorts = [{hostPort = 80;} {hostPort = 443;}];
+    containers.site-i.autoStart = true;
+    containers.site-i.macvlans = ["eth1"];
     containers.site-0.config = { pkgs, lib, ... }:
     {
       networking.firewall.allowedTCPPorts = [ 80 ];
